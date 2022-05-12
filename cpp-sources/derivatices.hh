f96f999336b243e74dccaa7863bf38d62e20ec4f
antoine.aubin <aubin.antoine@lilo.org>
2022-05-12 17:50:26 +0200

    Adapt derivatives to matrix type

--- cpp-sources/derivatices.hh
+++ cpp-sources/derivatices.hh
@@ -1,10 +1,14 @@
 #pragma once
 
-struct tuple_array {
-    float* arr1;
-    float* arr2;
+#include "construct/matrix.hh"
+#include <cstdint>
+
+template <typename T>
+struct tuple_matrix {
+    matrix<T>* mat1;
+    matrix<T>* mat2;
 };
 
-float* gauss_kernel(int size); 
-tuple_array gauss_derivative_kernels(int size);
-tuple_array gauss_derivatives(int* img, int size);
+matrix<float>* gauss_kernel(int size); 
+tuple_matrix<float> gauss_derivative_kernels(int size);
+tuple_matrix<uint8_t> gauss_derivatives(int* img, int size);
--- cpp-sources/derivatives.cpp
+++ cpp-sources/derivatives.cpp
@@ -1,85 +1,101 @@
+#include <cstdint>
 #include <stdlib.h>
 #include <cmath> 
 #include "derivatices.hh"
+#include "tools/convolve.hh"
 
 using namespace std;
 
-float* mgridY(int size) {
+matrix<float>* mgridY(int size) {
     int border = size+1;
-    float* meshGrid = (float *) malloc((border) * (border) * sizeof(float));
+    matrix<float>* meshGrid = new matrix<float>(border, border);
     
     for (int i = -size; i < border; i++) {
         for (int j = -size; j < border; j ++) {
-            meshGrid[i * border + j] = i;
+            (*meshGrid)[i * border + j] = i;
         }
     } 
     return meshGrid;
 }
 
-float* mgridX(int size) {
+matrix<float>* mgridX(int size) {
     int border = size+1;
-    float* meshGrid = (float *) malloc((border) * (border) * sizeof(float));
+    matrix<float>* meshGrid = new matrix<float>(border, border);
     
     for (int i = -size; i < border; i++) {
         for (int j = -size; j < border; j ++) {
-            meshGrid[i * border + j] = j;
+            (*meshGrid)[i * border + j] = j;
         }
     } 
     return meshGrid;
 }
 
-float* gauss_kernel(int size) {
-    float* yGrid = mgridY(size);
-    float* xGrid = mgridX(size); 
+matrix<float>* gauss_kernel(int size) {
+    matrix<float>* yGrid = mgridY(size);
+    matrix<float>* xGrid = mgridX(size); 
 
     float sigma = (float)size / 3; 
 
-    float* meshGrid = (float *) malloc(pow(size * 2 + 1, 2) * sizeof(float));
+    matrix<float>* meshGrid = new matrix<float>(size * 2 + 1, size * 2 + 1);
 
     for (int i = 0; i < (size * 2) + 1; i ++) {
         for (int j = 0; j < (size * 2) + 1; j ++) {
             int index = i * (size * 2 + 1) + j;
-            float x = xGrid[index];
-            float y = yGrid[index];
-            meshGrid[index] = exp(-(pow(x, 2) / 2 * pow(sigma, 2)) + pow(y, 2) / 2 * pow(sigma, 2)); 
+            float x = (*xGrid)[index];
+            float y = (*yGrid)[index];
+            (*meshGrid)[index] = exp(-(pow(x, 2) / 2 * pow(sigma, 2)) + pow(y, 2) / 2 * pow(sigma, 2)); 
         }
     }
 
-    free(yGrid); 
-    free(xGrid);
+    delete yGrid;
+    delete xGrid;
 
     return meshGrid; 
 }   
 
 
-tuple_array gauss_derivative_kernels(int size) {
-    float* yGrid = mgridY(size);
-    float* xGrid = mgridX(size); 
+tuple_matrix<float> gauss_derivative_kernels(int size) {
+    matrix<float>* yGrid = mgridY(size);
+    matrix<float>* xGrid = mgridX(size); 
 
 
-    float* gx = (float *) malloc(pow(size * 2 + 1, 2) * sizeof(float)); 
-    float* gy = (float *) malloc(pow(size * 2 + 1, 2) * sizeof(float));
+    matrix<float>* gx = new matrix<float>(size * 2 + 1, size * 2 + 1);
+    matrix<float>* gy = new matrix<float>(size * 2 + 1, size * 2 + 1);
 
     float sigma = (float)size / 3;
 
     for (int i = 0; i < (size * 2) + 1; i ++) {
         for (int j = 0; j < (size * 2) + 1; j ++) {
             int index = i * (size * 2 + 1) + j;
-            float x = xGrid[index];
-            float y = yGrid[index];
+            float x = (*xGrid)[index];
+            float y = (*yGrid)[index];
             float val = exp(-(pow(x, 2) / 2 * pow(sigma, 2)) + pow(y, 2) / 2 * pow(sigma, 2));
-            gx[index] = -x * val; 
-            gy[index] = -y * val;
+            (*gx)[index] = -x * val; 
+            (*gy)[index] = -y * val;
         }
     }
 
-    free(yGrid); 
-    free(xGrid);
+    delete yGrid;
+    delete xGrid;
 
-    tuple_array res {
+    tuple_matrix<float> res {
         gx, 
         gy,
     };
     
+    return res;
+}
+
+tuple_matrix<uint8_t> gauss_derivatives(matrix<uint8_t>* img, int size) {
+    tuple_matrix<float> gxy = gauss_derivative_kernels(size); 
+
+    matrix<uint8_t> * imx = convolve(img, gxy.mat1);
+    matrix<uint8_t> * imy = convolve(img, gxy.mat2);
+
+    tuple_matrix<uint8_t> res {
+        imx, 
+        imy,
+    };
+
     return res;
 }
