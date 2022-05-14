b349474574ffcb726568801226889903ee6c0136
Alexandre James <alexandre.james@epita.fr>
2022-05-14 18:14:07 +0200

    updated convolve

--- cpp-sources/tools/convolve.hh
+++ cpp-sources/tools/convolve.hh
@@ -4,19 +4,25 @@
 #include "../construct/matrix.hh"
 
 template <typename num1, typename num2>
-matrix<num1> *convolve(matrix<num1> *m1, matrix<num2> *m2) {
-    matrix<num1> *output = new matrix<num1>(m1->rows, m1->cols);
+matrix<float> *convolve(matrix<num1> *m1, matrix<num2> *m2) {
+    matrix<float> *output = new matrix<float>(m1->rows, m1->cols);
     int x_pad = m2->rows / 2;
     int y_pad = m2->cols / 2;
 
-    for (int i = x_pad; i < m1->rows - x_pad; i++) {
-        for (int j = y_pad; j < m1->cols - y_pad; j++) {
-            float sum = 0;
+    for (int i = 0; i < m1->rows; i++) {
+        for (int j = 0; j < m1->cols; j++) {
+            int sum = 0.0;
+            float conv = 0;
 
             for (int k = 0; k < m2->rows; k++) {
+                int x_pos = i - x_pad + k;
                 for (int l = 0; l < m2->cols; l++) {
-                    sum += (*m2)[k + l * m2->rows]
-                    * (*m1)[i - x_pad + k + (j - y_pad + l) * m1->rows];
+                    int y_pos = j - y_pad + l;
+                    num1 value = 128;
+                    if (x_pos >= 0 && y_pos >= 0 && x_pos < m1->rows && y_pos < m1->cols)
+                        value = (*m1)[x_pos + y_pos * m1->rows];
+                    sum += (*m2)[k + l * m2->rows];
+                    conv += (*m2)[k + l * m2->rows] * value;
                 }
             }
             (*output)[i + j * m1->rows] = sum;
